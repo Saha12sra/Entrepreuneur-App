26e43f6281d20f59174ca6bc250980e4d672fdbc
Saha12sra <74057039+Saha12sra@users.noreply.github.com>
2021-09-04 09:00:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,137 @@
+window.addEventListener("keydown", myKeyDown);
+
+function pwd(e) {
+
+    console.log(e);
+    key_pressed = e.keyCode;
+    console.log(key_pressed);
+
+    if (key_pressed == '82') {
+        document.getElementById("pass").innerHTML = "r*********";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+
+
+
+    if (key_pressed == '79') {
+        document.getElementById("pass").innerHTML = "ro*********";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+
+
+    if (key_pressed == '66') {
+        document.getElementById("pass").innerHTML = "rob********";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+
+
+
+    if (key_pressed == '66') {
+        document.getElementById("pass").innerHTML = "robb*******";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+
+
+
+    if (key_pressed == '69') {
+        document.getElementById("pass").innerHTML = "robbe******";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+
+
+
+    if (key_pressed == '82') {
+        document.getElementById("pass").innerHTML = "robber*****";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+
+
+
+    if (key_pressed == '83') {
+        document.getElementById("pass").innerHTML = "robbers****";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+
+
+
+
+    if (key_pressed == '70') {
+        document.getElementById("pass").innerHTML = "robbersf***";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+
+
+    if (key_pressed == '76') {
+        document.getElementById("pass").innerHTML = "robbersfl**";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+
+
+    if (key_pressed == '69') {
+        document.getElementById("pass").innerHTML = "robbersfle*";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+
+
+    if (key_pressed == '69') {
+        document.getElementById("pass").innerHTML = "robbersflee";
+        document.getElementById("error1").innerHTML = "";
+    }
+    else {
+        document.getElementById("error1").innerHTML = "ERROR--Intruder?";
+        document.getElementById("pass").innerHTML = "XXCODEXX";
+
+    }
+}
+
+
